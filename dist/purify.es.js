2f29c06a54e9374a7cbcd05cb901f86429b33ea4
Aaron Belenky <aaron.b@sealsecurity.io>
2024-11-03 14:08:08 +0200

    2.0.12-sp4

--- dist/purify.es.js
+++ dist/purify.es.js
@@ -207,7 +207,7 @@ function createDOMPurify() {
    * Version label, exposed for easier checks
    * if DOMPurify is up to date or not
    */
-  DOMPurify.version = '2.0.12-sp1';
+  DOMPurify.version = '2.0.12-sp4';
 
   /**
    * Array of elements that DOMPurify removed during sanitation.
